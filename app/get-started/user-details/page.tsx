2fe9299586d2918f11e82e771f712f0239f2bb5c
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-10-21 23:47:34 +0530

    feat: created the last step up stage which is the signup form

--- app/get-started/user-details/page.tsx
+++ app/get-started/user-details/page.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface UserDetailsProps {
+  name: string | undefined;
+  email: string | undefined;
+  phoneNumber: string | undefined;
+}
+
+const Page = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState<UserDetailsProps>({
+    name: "",
+    email: "",
+    phoneNumber: ""
+  });
+
+  const { name, email, phoneNumber } = formData;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   return (
-    <div>page</div>
+    <>
+      <div className="flex flex-col h-full">
+        <div className="flex-1 min-h-0 flex flex-col lg:flex-row">
+          <div className="w-full lg:w-1/2 flex flex-col overflow-y-auto h-full">
+            <div className="h-full flex flex-col p-4">
+              <div className="">
+                <div className="grid grid-cols-2 lg:grid-cols-[1fr_auto_1fr] items-center min-h-0 lg:min-h-20 p-4 w-full">
+                  <div className="flex items-center gap-2 w-20">
+                    <Image
+                      src="/Mocha-e1760632297719.webp"
+                      alt="Meetly"
+                      width={200}
+                      height={200}
+                      quality={100}
+                      priority
+                    />
+                  </div>
+                  <div className="hidden lg:flex items-center gap-6"></div>
+                  <div className="flex items-center justify-end"></div>
+                </div>
+              </div>
+              <div className="flex-1 min-h-0 overflow-y-auto">
+                <div className="h-full flex flex-col">
+                  <div className="flex-1 overflow-y-auto flex flex-col gap-6 text-center px-4 pt-10 pb-4">
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl text-[#2F1107] font-semibold">What is your name?</h1>
+                    <input type="text" value={name} id="name" name="name" onChange={handleChange} className="bg-muted px-5 py-2 outline border-0 rounded-full w-full h-12 text-[#2F1107] font-medium text-base" />
+                    <div className='pt-6 border-t border-[#f7f0f2]'>
+                      <h1 className="text-2xl md:text-3xl lg:text-4xl text-[#2F1107] font-semibold">What is your email?</h1>
+                      <input type="email" value={email} id="email" name="email" onChange={handleChange} className="bg-muted px-5 py-2 outline border-0 rounded-full w-full h-12 text-[#2F1107] font-medium text-base mt-6" />
+                    </div>
+                    <div className='py-6 border-t border-[#f7f0f2]'>
+                      <h1 className="text-2xl md:text-3xl lg:text-4xl text-[#2F1107] font-semibold">What is phone number?</h1>
+                      <input type="text" value={phoneNumber} id="phoneNumber" name="phoneNumber" onChange={handleChange} className="bg-muted px-5 py-2 outline border-0 rounded-full w-full h-12 text-[#2F1107] font-medium text-base mt-6" />
+                    </div>
+                  </div>
+                  <div className="p-4 bg-background">
+                    <button
+                      type="button"
+                      className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm md:text-base font-medium transition-all select-none bg-[#FFD100] text-[#2F1107] hover:bg-[#2F1107] hover:text-[#ffd100] h-12 px-4 py-2 rounded-full w-full duration-500 disabled:opacity-60 disabled:cursor-not-allowed cursor-pointer"
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hidden lg:flex lg:w-1/2 relative items-center justify-center overflow-hidden bg-muted">
+            <div className="absolute right-1/8 h-2/3 w-auto">
+              <Image
+                src="/colleagues-having-a-coffee-break-1024x752.webp"
+                alt="Meetly"
+                width={600}
+                height={600}
+                quality={100}
+                priority
+                className="w-full h-auto"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Page
